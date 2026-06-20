6e5d6a37cd869ad9afd9479b6d72b9ae17dda63a
Kinata Dewa Ariandi <dewadafug766@gmail.com>
2026-06-20 21:10:44 +0700

    migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,12 +10,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class, // 1. Buat roles dulu
             AdminSeeder::class,               // 2. Admin
-            DosenSeeder::class,               // 3. Dosen (butuh roles)
-            KelasSeeder::class,               // 4. Kelas (butuh dosen)
-            MahasiswaSeeder::class,           // 5. Mahasiswa (butuh kelas + dosen)
-            MatkulSeeder::class,              // 6. Matkul (butuh kelas + dosen)
-            NilaiSeeder::class,               // 7. Nilai (butuh mahasiswa + matkul)
-            AbsensiSeeder::class,             // 8. Absensi (butuh mahasiswa + matkul)
         ]);
     }
 }
